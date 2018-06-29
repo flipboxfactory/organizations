0fee06bc1bbff8493d500349859057fa0b7d5721
Doug St. John <3673451+dougstjohn@users.noreply.github.com>
2018-06-29 01:22:15 -0400

    load code-toggle js component globally

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,7 +1,3 @@
-import CodeToggle from './CodeToggle'
-//import OutboundButton from './OutboundButton'
-
 export default ({ Vue, options, router, siteData }) => {
-    Vue.component('code-toggle', CodeToggle)
-  //  Vue.component('OutboundButton', OutboundButton)
+    // Enhance...
 }
