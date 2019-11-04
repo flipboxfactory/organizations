0282270ce7cc56ef5d45fd66698feda313cb0757
Nate Iler <nate@flipboxdigital.com>
2019-11-04 14:59:38 -0700

    updating docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,6 @@ module.exports = {
     title: 'Organizations',
     description: 'Parent + child organization management for Craft CMS',
     base: '/',
-    theme: 'flipbox',
     themeConfig: {
         logo: '/icon.svg',
         docsRepo: 'flipboxfactory/organizations',
@@ -18,7 +17,6 @@ module.exports = {
             // any other languages you want to include in code toggles...
         },
         nav: [
-            {text: 'Details', link: 'https://flipboxfactory.com/craft-cms-plugins/organizations'},
             {text: 'Changelog', link: 'https://github.com/flipboxfactory/organizations/blob/master/CHANGELOG.md'},
             {text: 'Repo', link: 'https://github.com/flipboxfactory/organizations'}
         ],
